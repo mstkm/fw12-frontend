c126bb7b0fe8b4696e66ebdbba1fc1796b173ed1
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-06 21:06:17 +0700

    create signup route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
