eca874040374ce5fb058adb441bbe38ca4628afa
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-06 12:02:06 +0700

    use tailwind bakcground image

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -4,18 +4,21 @@ import {Eye} from 'react-feather';
 const SignIn = () => {
   return(
     <div className='flex font-[inter]'>
-      <div className="flex-[60%] hero">
-        <div className='flex flex-col justify-center items-center bg-[#2B156B] opacity-80 h-[100%]'>
+      {/* Left */}
+      <div className="flex-[60%] bg-[url('../images/image1.png')]">
+        <div className='flex flex-col justify-center items-center bg-[#2B156B] opacity-80 h-screen'>
           <div>
             <img src={require('../assets/images/tickitz1.png')} alt='Hero' />
           </div>
           <div className='text-5xl text-white opacity-80'>wait, watch, wow!</div>
         </div>
       </div>
+
+      {/* Right */}
       <div className="flex flex-col flex-[40%] justify-center p-10">
         <div className='text-5xl font-bold mb-4'>Sign In</div>
         <div className='text-[#AAAAAA] mb-10'>Sign in with your data that you entered during your registration</div>
-        <form className='mb-5'>
+        <form className='mb-8'>
           <div className='mb-5'>
             <div className='text-[#4E4B66] mb-2'>Email</div>
             <input className='w-[400px] h-[50px] border-[1px] border-[#DEDEDE] rounded-[16px] pl-4 focus:outline-none' placeholder='Write your email'></input>
@@ -31,7 +34,7 @@ const SignIn = () => {
             <button className='w-[400px] h-[50px] bg-[#5F2EEA] border-[1px] border-[#5F2EEA] rounded-[16px] pl-4 text-white'>Sign In</button>
           </div>         
         </form>
-        <div className='text-[#8692A6] text-center'>Forgot your password? <span className='text-[#5F2EEA] underline cursor-pointer'>Reset now</span></div>
+        <div className='text-[#8692A6] text-center mb-2'>Forgot your password? <span className='text-[#5F2EEA] underline cursor-pointer'>Reset now</span></div>
         <div className='text-[#8692A6] text-center'>Don't have an account? <span className='text-[#5F2EEA] underline cursor-pointer'>Sign Up</span></div>
       </div>
       
