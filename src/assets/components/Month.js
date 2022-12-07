b1b80850ac6358024c6f466f8107c8280b271b2b
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-07 19:57:56 +0700

    create month component

---
+++ src/assets/components/Month.js
@@ -0,0 +1,20 @@
+const Month = () => {
+  return(
+    <div className='flex flex-nowrap ml-[100px] py-5 overflow-x-scroll gap-4'>
+      <div className='flex justify-center items-center min-w-[127px] min-h-[40px] border-[1px] border-[#5F2EEA] text-[#5F2EEA] rounded-[4px] cursor-pointer active:bg-[#5F2EEA] active:text-white'>September</div>
+      <div className='flex justify-center items-center min-w-[127px] min-h-[40px] border-[1px] border-[#5F2EEA] text-[#5F2EEA] rounded-[4px] cursor-pointer active:bg-[#5F2EEA] active:text-white'>October</div>
+      <div className='flex justify-center items-center min-w-[127px] min-h-[40px] border-[1px] border-[#5F2EEA] text-[#5F2EEA] rounded-[4px] cursor-pointer active:bg-[#5F2EEA] active:text-white'>November</div>
+      <div className='flex justify-center items-center min-w-[127px] min-h-[40px] border-[1px] border-[#5F2EEA] text-[#5F2EEA] rounded-[4px] cursor-pointer active:bg-[#5F2EEA] active:text-white'>December</div>
+      <div className='flex justify-center items-center min-w-[127px] min-h-[40px] border-[1px] border-[#5F2EEA] text-[#5F2EEA] rounded-[4px] cursor-pointer active:bg-[#5F2EEA] active:text-white'>January</div>
+      <div className='flex justify-center items-center min-w-[127px] min-h-[40px] border-[1px] border-[#5F2EEA] text-[#5F2EEA] rounded-[4px] cursor-pointer active:bg-[#5F2EEA] active:text-white'>February</div>
+      <div className='flex justify-center items-center min-w-[127px] min-h-[40px] border-[1px] border-[#5F2EEA] text-[#5F2EEA] rounded-[4px] cursor-pointer active:bg-[#5F2EEA] active:text-white'>March</div>
+      <div className='flex justify-center items-center min-w-[127px] min-h-[40px] border-[1px] border-[#5F2EEA] text-[#5F2EEA] rounded-[4px] cursor-pointer active:bg-[#5F2EEA] active:text-white'>April</div>
+      <div className='flex justify-center items-center min-w-[127px] min-h-[40px] border-[1px] border-[#5F2EEA] text-[#5F2EEA] rounded-[4px] cursor-pointer active:bg-[#5F2EEA] active:text-white'>May</div>
+      <div className='flex justify-center items-center min-w-[127px] min-h-[40px] border-[1px] border-[#5F2EEA] text-[#5F2EEA] rounded-[4px] cursor-pointer active:bg-[#5F2EEA] active:text-white'>June</div>
+      <div className='flex justify-center items-center min-w-[127px] min-h-[40px] border-[1px] border-[#5F2EEA] text-[#5F2EEA] rounded-[4px] cursor-pointer active:bg-[#5F2EEA] active:text-white'>July</div>
+      <div className='flex justify-center items-center min-w-[127px] min-h-[40px] border-[1px] border-[#5F2EEA] text-[#5F2EEA] rounded-[4px] cursor-pointer active:bg-[#5F2EEA] active:text-white'>Augustus</div>
+    </div>
+  )
+}
+
+export default Month
