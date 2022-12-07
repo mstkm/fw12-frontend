46031b7de37dbfd5b19f737ebfcfb56a0be94967
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-07 19:58:53 +0700

    create showtimes and tickets component

---
+++ src/assets/components/ShowtimesTickets.js
@@ -0,0 +1,208 @@
+const ShowtimesTickets = () => {
+  return(
+    <>
+    <div className='grid grid-cols-3 gap-8'>
+      <div className="w-[350px] bg-white pb-5 font-[mulish] rounded-[8px]">
+        <div className="grid grid-cols-2 items-center px-5 py-5 border-b-[1px]">
+          <div>
+            <img className="w-[70%]" src={require('../images/ebv.id.png')} alt='ebv.id' />
+          </div>
+          <div>
+            <div className="font-bold text-lg">ebv.id</div>
+            <div className="text-sm">Whatever street No.12, South Purwokerto</div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-4 px-5 py-5 gap-2 text-sm">
+          <div>08:30am</div>
+          <div>10:30pm</div>
+          <div>12:00pm</div>
+          <div>02:00pm</div>
+          <div>04:30pm</div>
+          <div>07:00pm</div>
+          <div>08:30pm</div>
+        </div>
+
+        <div className="flex px-5 py-5 mb-3">
+          <div className="flex-1">Price</div>
+          <div className="font-bold">$10.00/seat</div>
+        </div>
+
+        <div className="px-5">
+          <button className="border-[1px] w-[100%] h-[40px] bg-[#5F2EEA] rounded-[4px] text-white">Book Now</button>
+        </div>
+      </div>
+
+      <div className="w-[350px] bg-white pb-5 font-[mulish] rounded-[8px]">
+        <div className="grid grid-cols-2 items-center px-5 py-5 border-b-[1px]">
+          <div>
+            <img className="w-[70%]" src={require('../images/cineone21.png')} alt='ebv.id' />
+          </div>
+          <div>
+            <div className="font-bold text-lg">CineOne21</div>
+            <div className="text-sm">Downcare street  No. 21, East Purwokerto</div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-4 px-5 py-5 gap-2 text-sm">
+          <div>08:30am</div>
+          <div>10:30pm</div>
+          <div>12:00pm</div>
+          <div>02:00pm</div>
+          <div>04:30pm</div>
+          <div>07:00pm</div>
+          <div>08:30pm</div>
+        </div>
+
+        <div className="flex px-5 py-5 mb-3">
+          <div className="flex-1">Price</div>
+          <div className="font-bold">$10.00/seat</div>
+        </div>
+
+        <div className="px-5">
+          <button className="border-[1px] w-[100%] h-[40px] bg-[#5F2EEA] rounded-[4px] text-white">Book Now</button>
+        </div>
+      </div>
+
+      <div className="w-[350px] bg-white pb-5 font-[mulish] rounded-[8px]">
+        <div className="grid grid-cols-2 items-center px-5 py-5 border-b-[1px]">
+          <div>
+            <img className="w-[70%]" src={require('../images/hiflix.png')} alt='ebv.id' />
+          </div>
+          <div>
+            <div className="font-bold text-lg">Hiflix</div>
+            <div className="text-sm">Colonel street No. 2, East Purwokerto</div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-4 px-5 py-5 gap-2 text-sm">
+          <div>08:30am</div>
+          <div>10:30pm</div>
+          <div>12:00pm</div>
+          <div>02:00pm</div>
+          <div>04:30pm</div>
+          <div>07:00pm</div>
+          <div>08:30pm</div>
+        </div>
+
+        <div className="flex px-5 py-5 mb-3">
+          <div className="flex-1">Price</div>
+          <div className="font-bold">$10.00/seat</div>
+        </div>
+
+        <div className="px-5">
+          <button className="border-[1px] w-[100%] h-[40px] bg-[#5F2EEA] rounded-[4px] text-white">Book Now</button>
+        </div>
+      </div>
+
+      <div className="w-[350px] bg-white pb-5 font-[mulish] rounded-[8px]">
+        <div className="grid grid-cols-2 items-center px-5 py-5 border-b-[1px]">
+          <div>
+            <img className="w-[70%]" src={require('../images/ebv.id.png')} alt='ebv.id' />
+          </div>
+          <div>
+            <div className="font-bold text-lg">ebv.id</div>
+            <div className="text-sm">Whatever street No.12, South Purwokerto</div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-4 px-5 py-5 gap-2 text-sm">
+          <div>08:30am</div>
+          <div>10:30pm</div>
+          <div>12:00pm</div>
+          <div>02:00pm</div>
+          <div>04:30pm</div>
+          <div>07:00pm</div>
+          <div>08:30pm</div>
+        </div>
+
+        <div className="flex px-5 py-5 mb-3">
+          <div className="flex-1">Price</div>
+          <div className="font-bold">$10.00/seat</div>
+        </div>
+
+        <div className="px-5">
+          <button className="border-[1px] w-[100%] h-[40px] bg-[#5F2EEA] rounded-[4px] text-white">Book Now</button>
+        </div>
+      </div>
+
+      <div className="w-[350px] bg-white pb-5 font-[mulish] rounded-[8px]">
+        <div className="grid grid-cols-2 items-center px-5 py-5 border-b-[1px]">
+          <div>
+            <img className="w-[70%]" src={require('../images/cineone21.png')} alt='ebv.id' />
+          </div>
+          <div>
+            <div className="font-bold text-lg">CineOne21</div>
+            <div className="text-sm">Downcare street  No. 21, East Purwokerto</div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-4 px-5 py-5 gap-2 text-sm">
+          <div>08:30am</div>
+          <div>10:30pm</div>
+          <div>12:00pm</div>
+          <div>02:00pm</div>
+          <div>04:30pm</div>
+          <div>07:00pm</div>
+          <div>08:30pm</div>
+        </div>
+
+        <div className="flex px-5 py-5 mb-3">
+          <div className="flex-1">Price</div>
+          <div className="font-bold">$10.00/seat</div>
+        </div>
+
+        <div className="px-5">
+          <button className="border-[1px] w-[100%] h-[40px] bg-[#5F2EEA] rounded-[4px] text-white">Book Now</button>
+        </div>
+      </div>
+
+      <div className="w-[350px] bg-white pb-5 font-[mulish] rounded-[8px]">
+        <div className="grid grid-cols-2 items-center px-5 py-5 border-b-[1px]">
+          <div>
+            <img className="w-[70%]" src={require('../images/hiflix.png')} alt='ebv.id' />
+          </div>
+          <div>
+            <div className="font-bold text-lg">Hiflix</div>
+            <div className="text-sm">Colonel street No. 2, East Purwokerto</div>
+          </div>
+        </div>
+
+        <div className="grid grid-cols-4 px-5 py-5 gap-2 text-sm">
+          <div>08:30am</div>
+          <div>10:30pm</div>
+          <div>12:00pm</div>
+          <div>02:00pm</div>
+          <div>04:30pm</div>
+          <div>07:00pm</div>
+          <div>08:30pm</div>
+        </div>
+
+        <div className="flex px-5 py-5 mb-3">
+          <div className="flex-1">Price</div>
+          <div className="font-bold">$10.00/seat</div>
+        </div>
+
+        <div className="px-5">
+          <button className="border-[1px] w-[100%] h-[40px] bg-[#5F2EEA] rounded-[4px] text-white">Book Now</button>
+        </div>
+      </div>
+    </div>
+
+    <div className='flex items-center my-8'>
+      <div className='flex-1'>
+        <div className='border-[1px] bg-[#DEDEDE]'></div>
+      </div>
+      <div className='text-[#5F2EEA] px-10 cursor-pointer hover:font-bold'>view more</div>
+      <div className='flex-1 relative'>
+        <div className='border-[1px] bg-[#DEDEDE]'></div>
+      </div>
+    </div>
+    </>
+
+
+
+  )
+}
+
+export default ShowtimesTickets
