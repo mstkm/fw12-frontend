eafebc53a82643a71c5a0f7b6eb23554fa886073
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-07 09:34:17 +0700

    add forgot and update password route

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
+import ForgotPassword from './pages/ForgotPassword';
+import UpdatePassword from './pages/UpdatePassword';
 
 const App = () => {
   return (
@@ -11,6 +13,8 @@ const App = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/forgotPassword" element={<ForgotPassword />} />
+        <Route path="/updatePassword" element={<UpdatePassword />} />
       </Routes>
     </BrowserRouter>
   );
