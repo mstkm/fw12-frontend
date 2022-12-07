8d9ad6cbef9cbf1a01eebd50f6d5b23376beb6c2
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-07 12:50:47 +0700

    add Now component

--- src/assets/components/Now.js
+++ src/assets/components/Now.js
@@ -3,35 +3,31 @@ import axios from 'axios';
 
 
 function Now() {
-  const [movies, setMovies] = React.useState({});
+  const [nowMovies, setNowMovies] = React.useState({});
   React.useEffect(() => {
-    getMovies().then((data) => {
-      setMovies(data)
+    getNowMovies().then((data) => {
+      setNowMovies(data)
     })
   }, []);
 
 
-  const getMovies = async () => {
-    const {data} = await axios.get('http://localhost:8888/movies/nowShowing?limit=10');
+  const getNowMovies = async () => {
+    const {data} = await axios.get('http://localhost:8888/movies/nowShowing?limit=8');
     return data;
   }
 
   return(
     <div className='flex flex-nowrap mt-10 gap-6 ml-[100px] overflow-x-scroll'>
-      {/* <div className='min-w-fit p-6 border-2 border-white rounded-[8px]'>
-        <img src={require('../images/spiderman.png')} alt='Spiderman' />
-      </div> */}
-      
-      {movies?.results?.map((movie, index) => {
+      {nowMovies?.results?.map((movie, index) => {
         return(
           <React.Fragment key={String(index)}>
             <div className='min-w-fit p-6 border-2 border-white rounded-[8px]'>
               <img className='w-[159px] h-[244px] rounded-[8px]' src={movie.picture} alt={movie.title} />
-            </div> 
+            </div>
         </React.Fragment>
         )
       })}
-      
+
     </div>
   )
 }
