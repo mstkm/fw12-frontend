bbd475e043592d4f3d846a93ab5d577ebf94ac76
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-07 19:57:00 +0700

    create footer component

---
+++ src/assets/components/Footer.js
@@ -0,0 +1,63 @@
+import { Facebook, Instagram, Twitter, Youtube } from 'react-feather';
+
+const Footer = () => {
+  return(
+    <div className='font-[mulish] px-[100px] py-10'>
+        <div className='flex gap-8'>
+          <div className='flex-1'>
+            <div className='mb-4'>
+              <img src={require('../images/logo.png')} alt='Logo'/>
+            </div>
+            <div className='text-[#6E7191] text-sm'>Stop waiting in line. Buy tickets conveniently, watch movies quietly.</div>
+          </div>
+          <div className='flex-1'>
+            <div className='text-lg font-bold mb-4'>Explore</div>
+            <div className='text-[#6E7191] text-sm mb-2'>Home</div>
+            <div className='text-[#6E7191] text-sm'>List Movie</div>
+          </div>
+          <div className='flex-1'>
+            <div className='text-lg font-bold mb-4'>Our Sponsor</div>
+            <div className='mb-3'>
+              <img src={require('../images/ebv.id.png')} alt='ebv.id' />
+            </div>
+            <div className='mb-3'>
+              <img src={require('../images/cineone21.png')} alt='CineOne21' />
+            </div>
+            <div className='mb-3'>
+              <img src={require('../images/hiflix.png')} alt='Hiflix' />
+            </div>
+          </div>
+          <div className='flex-1'>
+            <div className='text-lg font-bold mb-4'>Follow us</div>
+            <div className='flex items-center text-[#6E7191] text-sm mb-3'>
+              <div className='mr-4'>
+                <Facebook />
+              </div>
+              <div>Tickitz Cinema id</div>
+            </div>
+            <div className='flex items-center text-[#6E7191] text-sm mb-3'>
+              <div className='mr-4'>
+                <Instagram />
+              </div>
+              <div>tickitz.id</div>
+            </div>
+            <div className='flex items-center text-[#6E7191] text-sm mb-3'>
+              <div className='mr-4'>
+                <Twitter />
+              </div>
+              <div>tickitz.id</div>
+            </div>
+            <div className='flex items-center text-[#6E7191] text-sm mb-3'>
+              <div className='mr-4'>
+                <Youtube />
+              </div>
+              <div>Tickitz Cinema id</div>
+            </div>
+          </div>
+        </div>
+        <div className='mt-[60px] text-center text-[#6E7191] text-sm'>© 2020 Tickitz. All Rights Reserved.</div>
+      </div>
+  )
+}
+
+export default Footer
