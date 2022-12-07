4cd226ff61cf2aae52b8b88fa4c67837c48a943c
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-07 07:51:08 +0700

    change styling

--- src/pages/SignIn.js
+++ src/pages/SignIn.js
@@ -1,41 +1,42 @@
 // import React from 'react';
 import {Eye} from 'react-feather';
+import {Link} from 'react-router-dom';
 
 const SignIn = () => {
   return(
-    <div className='auth-wrapper'>
+    <div className='flex font-[inter] h-screen'>
       {/* Left */}
-      <div className='left-auth'>
-        <div className='bg-layer'>
+      <div className="flex-[60%] bg-[url('../images/image1.png')] bg-cover max-[426px]:hidden">
+        <div className='flex flex-col justify-center items-center bg-[#2B156B] opacity-80 h-screen'>
           <div className='flex justify-center'>
             <img className='max-[769px]:w-[80%]' src={require('../assets/images/tickitz1.png')} alt='Logo' />
           </div>
-          <div className='text-banner'>wait, watch, wow!</div>
+          <div className='flex justify-center text-5xl text-white opacity-80 max-[769px]:text-[2rem]'>wait, watch, wow!</div>
         </div>
       </div>
 
       {/* Right */}
-      <div className="right-auth max-[425.98px]:py-[19rem] max-[768.98px]:overflow-y-scroll max-[768.98px]:pt-[10rem] max-[768.98px]:pb-[2rem]">
-        <div className='text-h'>Sign In</div>
-        <div className='text-p'>Sign in with your data that you entered during your registration</div>
-        <form className='form-auth'>
-          <div className='input-wrapper'>
-            <div className='label-input'>Email</div>
-            <input className='input' placeholder='Write your email'></input>
+      <div className="flex flex-col flex-[40%] justify-center px-10 max-[425.98px]:py-[18rem] max-[768.98px]:pt-[10rem] max-[768.98px]:pb-[2rem] max-[768.98px]:overflow-y-scroll">
+        <div className='text-5xl font-bold mb-4'>Sign In</div>
+        <div className='text-[#AAAAAA] mb-10'>Sign in with your data that you entered during your registration</div>
+        <form className='mb-8'>
+          <div className='mb-5'>
+            <div className='text-[#4E4B66] mb-2'>Email</div>
+            <input className='w-[100%] h-[50px] border-[1px] border-[#DEDEDE] rounded-[16px] pl-4 focus:outline-none' placeholder='Write your email'></input>
           </div>
-          <div className='input-wrapper'>
-            <div className='label-input'>Password</div>
+          <div className='mb-5'>
+            <div className='text-[#4E4B66] mb-2'>Password</div>
             <div className='relative'>
               <Eye className='absolute bottom-3 right-[15px]'/>
-              <input className='input' placeholder='Write your password'></input>
+              <input className='w-[100%] h-[50px] border-[1px] border-[#DEDEDE] rounded-[16px] pl-4 focus:outline-none' placeholder='Write your password'></input>
             </div>
           </div> 
           <div className='mt-10'>
-            <button className='button-auth'>Sign In</button>
+            <button className='w-[100%] h-[50px] bg-[#5F2EEA] border-[1px] border-[#5F2EEA] rounded-[16px] pl-4 text-white'>Sign In</button>
           </div>         
         </form>
-        <div className='text-[#8692A6] text-center mb-2'>Forgot your password? <span className='link'>Reset now</span></div>
-        <div className='text-[#8692A6] text-center'>Don't have an account? <span className='link'>Sign Up</span></div>
+        <div className='text-[#8692A6] text-center mb-2'>Forgot your password? <span className='text-[#5F2EEA] underline cursor-pointer hover:font-bold'>Reset now</span></div>
+        <div className='text-[#8692A6] text-center'>Don't have an account? <Link to='/signup' className='text-[#5F2EEA] underline cursor-pointer hover:font-bold'>Sign Up</Link></div>
       </div>
       
     </div>
--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -3,50 +3,50 @@ import {Link} from 'react-router-dom';
 
 function SignUp() {
   return(
-    <div className='auth-wrapper'>
+    <div className='flex font-[inter] h-screen'>
       {/* Left */}
-      <div className='left-auth'>
-        <div className='bg-layer'>
+      <div className='flex-[60%] bg-[url("../images/image1.png")] bg-cover max-[426px]:hidden'>
+        <div className='flex flex-col justify-center items-center bg-[#2B156B] opacity-80 h-screen'>
           <div className='flex justify-center'>
             <img className='max-[769px]:w-[80%]' src={require('../assets/images/tickitz1.png')} alt='Logo' />
           </div>
-          <div className='text-banner'>wait, watch, wow!</div>
+          <div className='flex justify-center text-5xl text-white opacity-80 max-[769px]:text-[2rem]'>wait, watch, wow!</div>
         </div>
       </div>
 
       {/* Right */}
-      <div className='right-auth overflow-y-scroll'>
-        <div className='text-h mt-[18rem]'>Sign Up</div>
-        <div className='text-p'>Fill your additional details</div>
-        <form className='form-auth'>
-          <div className='input-wrapper'>
-          <div className='label-input'>First Name</div>
-            <input className='input' placeholder='Write your first name'></input>
+      <div className='flex flex-col flex-[40%] justify-center px-10 overflow-y-scroll'>
+        <div className='text-5xl font-bold mb-4 mt-[18rem] max-[425.98px]:mt-[20rem] max-[768.98px]:mt-[25rem]'>Sign Up</div>
+        <div className='text-[#AAAAAA] mb-10'>Fill your additional details</div>
+        <form className='mb-8'>
+          <div className='mb-5'>
+          <div className='text-[#4E4B66] mb-2'>First Name</div>
+            <input className='w-[100%] h-[50px] border-[1px] border-[#DEDEDE] rounded-[16px] pl-4 focus:outline-none' placeholder='Write your first name'></input>
           </div>
-          <div className='input-wrapper'>
-          <div className='label-input'>Last Name</div>
-            <input className='input' placeholder='Write your last name'></input>
+          <div className='mb-5'>
+          <div className='text-[#4E4B66] mb-2'>Last Name</div>
+            <input className='w-[100%] h-[50px] border-[1px] border-[#DEDEDE] rounded-[16px] pl-4 focus:outline-none' placeholder='Write your last name'></input>
           </div>
-          <div className='input-wrapper'>
-          <div className='label-input'>Phone Number</div>
-            <input className='input' placeholder='Write your phone number'></input>
+          <div className='mb-5'>
+          <div className='text-[#4E4B66] mb-2'>Phone Number</div>
+            <input className='w-[100%] h-[50px] border-[1px] border-[#DEDEDE] rounded-[16px] pl-4 focus:outline-none' placeholder='Write your phone number'></input>
           </div>
-          <div className='input-wrapper'>
-            <div className='label-input'>Email</div>
-            <input className='input' placeholder='Write your email'></input>
+          <div className='mb-5'>
+            <div className='text-[#4E4B66] mb-2'>Email</div>
+            <input className='w-[100%] h-[50px] border-[1px] border-[#DEDEDE] rounded-[16px] pl-4 focus:outline-none' placeholder='Write your email'></input>
           </div>
-          <div className='input-wrapper'>
-            <div className='label-input'>Password</div>
+          <div className='mb-5'>
+            <div className='text-[#4E4B66] mb-2'>Password</div>
             <div className='relative'>
               <Eye className='absolute top-3 right-[15px]'/>
-              <input className='input' placeholder='Write your password'></input>
+              <input className='w-[100%] h-[50px] border-[1px] border-[#DEDEDE] rounded-[16px] pl-4 focus:outline-none' placeholder='Write your password'></input>
             </div>
           </div> 
           <div className='mt-10'>
-            <button className='button-auth'>Sign Up</button>
+            <button className='w-[100%] h-[50px] bg-[#5F2EEA] border-[1px] border-[#5F2EEA] rounded-[16px] pl-4 text-white'>Sign Up</button>
           </div>         
         </form>
-        <div className='text-[#8692A6] text-center'>Already have an account? <Link to='/signin' className='link'>Sign In</Link></div>
+        <div className='text-[#8692A6] text-center mb-10'>Already have an account? <Link to='/signin' className='text-[#5F2EEA] underline cursor-pointer hover:font-bold'>Sign In</Link></div>
       </div>
     </div>
   )
