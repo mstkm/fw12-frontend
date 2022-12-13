40523c1a7dd23e8c508abd17f153f25dd35510b2
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-13 08:05:17 +0700

    change styling now showing movie

--- src/assets/components/NowShowing.js
+++ src/assets/components/NowShowing.js
@@ -22,13 +22,15 @@ function NowShowing() {
       {nowShowingMovies?.results?.map((movie, index) => {
         return(
           <React.Fragment key={String(index)}>
-            <div className='min-w-fit p-6 border-2 border-white rounded-[8px]'>
-              <img className='w-[159px] h-[244px] rounded-[8px]' src={movie.picture} alt={movie.title} />
-              <div className='w-[159px] text-center mt-5'>
+            <div className='group flex flex-col items-center min-w-fit  hover:bg-white hover:border-2 hover:border-[#F5F6F8] rounded-[8px]'>
+              <div className='min-w-fit p-6 border-2 border-white rounded-[8px]'>
+                <img className='w-[159px] h-[244px] rounded-[8px]' src={movie.picture} alt={movie.title} />
+              </div>
+              <div className='w-[159px] text-center bg-white hidden group-hover:block'>
                 <div className='font-bold whitespace-nowrap overflow-hidden text-ellipsis'>{movie.title}</div>
                 <div className='font-medium text-sm text-[#A0A3BD] whitespace-nowrap overflow-hidden text-ellipsis'>{movie.genre}</div>
                 <div>
-                  <Link to={'/movieDetails/'+movie.id}><button className='w-[100%] mt-7 p-1 border-[0.5px] border-[#5F2EEA] rounded-[4px] text-[#5F2EEA] hover:bg-[#5F2EEA] hover:text-white'>Detail</button></Link>
+                  <Link to={'/movieDetails/'+movie.id}><button className='w-[100%] my-5 p-1 border-[0.5px] border-[#5F2EEA] rounded-[4px] text-[#5F2EEA] hover:bg-[#5F2EEA] hover:text-white'>Detail</button></Link>
                 </div>
               </div>
             </div>
