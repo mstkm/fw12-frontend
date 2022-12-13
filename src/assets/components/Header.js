9a39735751a49349e81872136f6220ac449d2911
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-13 11:00:12 +0700

    add useNavigate for dropdown photo profile

--- src/assets/components/Header.js
+++ src/assets/components/Header.js
@@ -2,49 +2,30 @@ import React from 'react';
 import { Search } from 'react-feather';
 import { useNavigate } from 'react-router-dom';
 
-const DropSearch = () => {
-  return(
-    <div id='searchMovie' className='flex items-center relative'>
-      <div className='absolute top-3.5 left-3 cursor-pointer'><Search /></div>
-      <input className='w-[250px] h-[50px] border-[1px] border-[#DEDED] rounded-[16px] bg-[#FCFDFE] pl-12 focus:outline-none' placeholder='Search Movie Name ...'></input>
-  </div>
-  )
-}
-
-const DropPhotoProfile = () => {
-  return(
-    <div id='drop-profile' className='absolute top-[50px] right-1 border-[1px] border-[#DEDED] rounded-[2px] bg-[#FCFDFE] pl-2 pr-8 pt-1 pb-3'>
-    <div className='mb-3 cursor-pointer hover:font-bold'>Profile</div>
-    <div className='cursor-pointer hover:font-bold'>Logout</div>
-  </div>
-  )
-}
-
 const Header = () => {
   const navigate = useNavigate()
   const directToHome = () => {
     navigate('/home')
   }
-
+  const directToProfile = () => {
+    navigate('/profile')
+  }
+  const directLogout = () => {
+    navigate('/')
+  }
   const directToViewAll = () => {
     navigate('/viewAll')
   }
 
-  const [clickSearch, getClickSearch] = React.useState(false)
+  const [dropSearch, setDropSearch] = React.useState(false)
   const showDropSearch = () => {
-    if (clickSearch === false) {
-      getClickSearch(true)
-    } else {
-      getClickSearch(false)
+    if (dropSearch === false) {
+      setDropSearch(true)
     }
   }
-
-  const [clickPhotoProfile, getClickPhotoProfile] = React.useState(false)
-  const showDropPhotoProfile = () => {
-    if (clickPhotoProfile === false) {
-      getClickPhotoProfile(true)
-    } else {
-      getClickPhotoProfile(false)
+  const hiddenDropSearch = () => {
+    if (dropSearch === true) {
+      setDropSearch(false)
     }
   }
 
@@ -56,16 +37,22 @@ const Header = () => {
         <div onClick={directToViewAll} className='cursor-pointer hover:font-bold'>List Movie</div>
       </div>
 
-      <div className='relative w-[250px] h-[50px] mr-[50px]'>
-        {clickSearch ? <DropSearch /> : false}
-        <div onClick={showDropSearch} className='absolute cursor-pointer top-3.5 right-1'><Search /></div>
+      <div onMouseLeave={hiddenDropSearch} className='relative w-[250px] h-[50px] mr-[50px]'>
+        {dropSearch ? <div className='flex items-center relative z-10'>
+          <div className='absolute top-3.5 left-3 cursor-pointer'><Search /></div>
+          <input className='w-[250px] h-[50px] border-[1px] border-[#DEDED] rounded-[16px] bg-[#FCFDFE] pl-12 focus:outline-none' placeholder='Search Movie Name ...'></input>
+        </div> : false}
+        <div onMouseEnter={showDropSearch} className='absolute cursor-pointer top-3.5 right-1'><Search /></div>
       </div>
 
       <div className='relative'>
-        <div onClick={showDropPhotoProfile} className='cursor-pointer'>
+        <div className='group cursor-pointer'>
           <img className='w-[50px] h-[50px] rounded-full' src='https://picsum.photos/200/300' alt='foto-profil'/>
+          <div id='drop-profile' className='group-hover:block hidden absolute top-[50px] right-1 border-[1px] border-[#DEDED] rounded-[2px] bg-[#FCFDFE] pl-2 pr-8 pt-1 pb-3'>
+          <div onClick={directToProfile} className='mb-3 cursor-pointer hover:font-bold'>Profile</div>
+          <div onClick={directLogout} className='cursor-pointer hover:font-bold'>Logout</div>
         </div>
-        {clickPhotoProfile ? <DropPhotoProfile /> : false}
+        </div>
       </div>
     </div>
   )
