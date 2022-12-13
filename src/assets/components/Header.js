87593b4ae7d6efeecefa21a183c2c90488728524
mstkm <99125333+mstkm@users.noreply.github.com>
2022-12-13 07:26:02 +0700

    add conditional rendering for search and profile

--- src/assets/components/Header.js
+++ src/assets/components/Header.js
@@ -1,7 +1,24 @@
-// import { useNavigate } from 'react-router-dom';
+import React from 'react';
 import { Search } from 'react-feather';
 import { useNavigate } from 'react-router-dom';
 
+const DropSearch = () => {
+  return(
+    <div id='searchMovie' className='flex items-center relative'>
+      <div className='absolute top-3.5 left-3 cursor-pointer'><Search /></div>
+      <input className='w-[250px] h-[50px] border-[1px] border-[#DEDED] rounded-[16px] bg-[#FCFDFE] pl-12 focus:outline-none' placeholder='Search Movie Name ...'></input>
+  </div>
+  )
+}
+
+const DropPhotoProfile = () => {
+  return(
+    <div id='drop-profile' className='absolute top-[50px] right-1 border-[1px] border-[#DEDED] rounded-[2px] bg-[#FCFDFE] pl-2 pr-8 pt-1 pb-3'>
+    <div className='mb-3 cursor-pointer hover:font-bold'>Profile</div>
+    <div className='cursor-pointer hover:font-bold'>Logout</div>
+  </div>
+  )
+}
 
 const Header = () => {
   const navigate = useNavigate()
@@ -12,6 +29,25 @@ const Header = () => {
   const directToViewAll = () => {
     navigate('/viewAll')
   }
+
+  const [clickSearch, getClickSearch] = React.useState(false)
+  const showDropSearch = () => {
+    if (clickSearch === false) {
+      getClickSearch(true)
+    } else {
+      getClickSearch(false)
+    }
+  }
+
+  const [clickPhotoProfile, getClickPhotoProfile] = React.useState(false)
+  const showDropPhotoProfile = () => {
+    if (clickPhotoProfile === false) {
+      getClickPhotoProfile(true)
+    } else {
+      getClickPhotoProfile(false)
+    }
+  }
+
   return(
     <div className='flex items-center px-[100px] py-7 font-[mulish]'>
       <div className='flex flex-1 items-center'>
@@ -21,21 +57,15 @@ const Header = () => {
       </div>
 
       <div className='relative w-[250px] h-[50px] mr-[50px]'>
-        <div id='searchMovie' className='flex items-center relative hidden'>
-          <div className='absolute top-3.5 left-3 cursor-pointer'><Search /></div>
-          <input className='w-[250px] h-[50px] border-[1px] border-[#DEDED] rounded-[16px] bg-[#FCFDFE] pl-12 focus:outline-none' placeholder='Search Movie Name ...'></input>
-        </div>
-        <div className='absolute cursor-pointer top-3.5 right-1'><Search /></div>
+        {clickSearch ? <DropSearch /> : false}
+        <div onClick={showDropSearch} className='absolute cursor-pointer top-3.5 right-1'><Search /></div>
       </div>
 
       <div className='relative'>
-        <div className='cursor-pointer'>
+        <div onClick={showDropPhotoProfile} className='cursor-pointer'>
           <img className='w-[50px] h-[50px] rounded-full' src='https://picsum.photos/200/300' alt='foto-profil'/>
         </div>
-        <div id='drop-profile' className='absolute top-[50px] right-1 border-[1px] border-[#DEDED] rounded-[2px] bg-[#FCFDFE] pl-2 pr-8 pt-1 pb-3 hidden'>
-          <div className='mb-3 cursor-pointer hover:font-bold'>Profile</div>
-          <div className='cursor-pointer hover:font-bold'>Logout</div>
-        </div>
+        {clickPhotoProfile ? <DropPhotoProfile /> : false}
       </div>
     </div>
   )
