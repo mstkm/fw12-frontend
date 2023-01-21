8329692fb13563f3bd2fbc5e3e8978152704a3ba
mstkm <99125333+mstkm@users.noreply.github.com>
2023-01-21 08:31:27 +0700

    implement eslint

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    './src/**/*.{js,jsx,ts,tsx}'
   ],
   theme: {
     extend: {
       colors: {
-        'primary': '#00005C',
+        primary: '#00005C'
       },
       backgroundColor: {
-        'primary': '#00005C'
+        primary: '#00005C'
       }
-    },
+    }
   },
-  plugins: [require("daisyui")],
+  plugins: [require('daisyui')]
 }
